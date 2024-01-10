75462634a0a1acbb4cf3350ad0b9caf58b0b8ee9
TheCedarPrince <jacobszelko@gmail.com>
2024-01-10 11:04:51 -0500

    Initial prototype of Metrics package

--- src/OMOPCDMCohortMetrics.jl
+++ src/OMOPCDMCohortMetrics.jl
@@ -1,5 +1,37 @@
 module OMOPCDMCohortMetrics
 
-# Write your package code here.
+using DataFrames
+using OMOPCDMCohortCreator
+
+import Base.Iterators:
+    product
+
+include("./fairness.jl")
+
+function pairwise_overlapping_subjects(cohorts, conn)
+
+    if isa(cohorts, Vector)
+        nothing
+    else 
+        cohorts = [cohorts]
+    end
+
+    pairwise_matrix = zeros(length(cohorts), length(cohorts))
+
+    cohort_pairs = product(repeat([cohorts], 2)...) 
+
+    for cohort_pair in cohort_pairs
+        groups = groupby(GetCohortSubjects(cohort_pair, conn), :cohort_definition_id)
+        if length(groups) == 2
+            intersecting_subjects = length(intersect(groups[1].subject_id, groups[2].subject_id))
+        else
+            intersecting_subjects = length(groups[1].subject_id)
+        end
+        pairwise_matrix[cohort_pair...] = intersecting_subjects
+    end
+
+    return pairwise_matrix
+
+end
 
 end
