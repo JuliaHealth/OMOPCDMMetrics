5ff9e9a527f1e5b5425939db71f5b08f853ccf70
Jacob Zelko <zelko.j@northeastern.edu>
2024-08-19 17:12:14 -0400

    Starting a refactor of fairness code

--- src/fairness.jl
+++ src/fairness.jl
@@ -128,6 +128,74 @@ module Fairness
         return dps
     end
 
+    function _demographic_parity(cohorts::DataFrame, funcs, conn, reference_subjects, process_size, silver)
+
+        _funcs = [Fix2(fun, conn) for fun in funcs]
+
+        if isempty(reference_subjects)
+            reference_subjects = GetDatabasePersonIDs(conn)
+        end
+
+        println("Getting ready to run")
+        println(_funcs[1](1))
+
+        cohorts_df = cohorts
+        cohorts = cohorts.subject_id
+
+        subsets = _subset_subjects(reference_subjects, process_size)
+
+        denom = DataFrame()
+        for (idx, sub) in enumerate(subsets)
+            println("Denom Iteration is running: $idx")
+            denom = vcat(denom, _counter_reducer(sub, :count_denom, _funcs))
+        end
+
+        denom = groupby(denom, Not(:count_denom)) |> 
+        x -> combine(x, :count_denom => sum => :count_denom)
+
+        println("Calculating Numerator")
+        num = DataFrame()
+        for cohort_idx in unique(cohorts_df.cohort_definition_id)
+            subjects = filter(row -> row.cohort_definition_id == cohort_idx, cohorts_df).subject_id
+            subsets = _subset_subjects(subjects, process_size)
+            for sub in subsets
+                vals = _counter_reducer(sub, :count_num, _funcs)
+                vals.cohort_definition_id .= cohort_idx
+                num = vcat(num, vals)
+            end
+        end
+
+        println("Skipping Silver Calculation")
+        if silver == true
+            _, true_subjects, _ =  _overlapped_subjects(cohorts, conn)
+
+            subsets = _subset_subjects(true_subjects, process_size)
+
+            silver = DataFrame()
+            for sub in subsets
+                silver = vcat(silver, _counter_reducer(sub, :count_num, _funcs))
+            end
+
+            silver.cohort_definition_id .= :silver
+            silver = groupby(silver, Not(:count_num)) |> 
+            x -> combine(x, :count_num => sum => :count_num)
+
+            num = vcat(num, silver)
+
+        end
+
+        num = groupby(num, Not(:count_num)) |> 
+        x -> combine(x, :count_num => sum => :count_num)
+
+        dps = outerjoin(num, denom; on = names(num)[1:end-2] .|> 
+        x -> Symbol(x) => Symbol(x)) |>
+        x -> coalesce.(x, 0)
+
+        dps.demographic_parity = dps.count_num ./ dps.count_denom
+
+        return dps
+    end
+
     function _demographic_parity(cohorts::Vector{<:Any}, funcs, conn, reference_subjects, process_size)
 
         _funcs = [Fix2(fun, conn) for fun in funcs]
@@ -175,12 +243,16 @@ module Fairness
             reference_subjects = GetDatabasePersonIDs(conn)
         end
 
+        println("Getting ready to run")
+        println(_funcs[1](1))
+
         cohorts = cohorts.subject_id
 
         subsets = _subset_subjects(reference_subjects, process_size)
 
         denom = DataFrame()
-        for sub in subsets
+        for (idx, sub) in enumerate(subsets)
+            @info "Denom Iteration $idx"
             denom = vcat(denom, _counter_reducer(sub, :count_denom, _funcs))
         end
 
@@ -190,7 +262,8 @@ module Fairness
         subsets = _subset_subjects(cohorts, process_size)
 
         num = DataFrame()
-        for sub in subsets
+        for (idx, sub) in enumerate(subsets)
+            @info "Num Iteration $idx"
             num = vcat(num, _counter_reducer(sub, :count_num, _funcs))
         end
 
