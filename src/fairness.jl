93f5e8451e5468a8950f23df057f8bbcf423e6ea
TheCedarPrince <jacobszelko@gmail.com>
2024-08-21 12:55:12 -0400

    Revamping docstrings for code

--- src/fairness.jl
+++ src/fairness.jl
@@ -5,6 +5,13 @@ module Fairness
     import Base:
         Fix2
 
+    """
+    _counter_reducer(sub, count_name, funcs)
+
+
+
+
+    """
     function _counter_reducer(sub, count_name, funcs)
         for fun in funcs
             sub = fun(sub)
@@ -14,7 +21,28 @@ module Fairness
         x -> combine(x, nrow => count_name)
     end
 
-    function _subset_subjects(vec, process_size)
+    """
+    _subset_subjects(vec::Vector, subset_length::Int; full_subset::Bool = true)
+
+    Internal function that accepts a vector, subsets it, and returns a vector of vectors (the subsets of the original vector).
+
+    # Arguments:
+    
+    - `vec::Vector` - a vector of elements to subset
+
+    - `subset_length::Int` - how long each subset of the vector should be
+
+    # Keywork Arguments:
+    
+    - `full_subset::Bool` - completely subsets the input vector even if the input vector is not divisible by the given `subset_length` (Default: `true`)
+
+    # Returns:
+    
+    `subsets::Vector{Vector}` - a vector containing subsets of the original vector, `vec`, with each subset of the desired `subset_length`. (NOTE: If `full_subset` is set to `true`, the last element of this vector may be smaller then the `subset_length`). 
+
+    """
+    function _subset_subjects(vec::Vector, subset_length::Int; full_subset::Bool = true)
+        vec_length = size(vec)[1]
         subsets = []
         for i in 1:process_size:size(vec)[1]
             if i + process_size > size(vec)[1]
@@ -27,10 +55,41 @@ module Fairness
         return subsets
     end
 
-    function _overlapped_subjects(cohorts, conn)
+    """
+    _overlapped_subjects(cohorts::Vector, conn; patient_overlaps::Symbol = :default)
+
+    Internal function which accepts patient cohorts and determines total unique subjects between cohorts, which patients overlap between cohorts, and which patients do not overlap between cohorts.
+
+    # Arguments:
     
-        # Requirement is based on majority of phenotypes provided
-        required_overlapping_phenotypes = ceil(length(cohorts) / 2)
+    - `cohorts::Vector` - a vector of cohort IDs
+
+    - `conn` - database connection using DBInterface
+
+    # Keyword Arguments:
+    
+    - `patient_overlaps::Symbol` - determines how subjects are defined as overlapping between cohorts. Available options: `:default`. (NOTE: See References for details on options)
+
+    # Returns
+
+    A triple in the order, `total_subjects, intersecting_subjects, nonintersecting_subjects` and detailed as follows:
+    
+    - `total_subjects::Vector` - the total unique subject IDs between all cohorts
+
+    - `intersecting_subjects::Vector` - the subject IDs who overlap between cohorts
+
+    - `nonintersecting_subjects::Vector` - the subject IDs who do not overlap between cohorts
+
+    # References: 
+
+    The `:default` algorithm to calculate patient overlaps via `patient_overlaps` is from _T. Y. Sun, S. Bhave, J. Altosaar, and N. Elhadad, “Assessing Phenotype Definitions for Algorithmic Fairness,” arXiv:2203.05174 [cs, q-bio], Mar. 2022, Accessed: Apr. 29, 2022. [Online]. Available: http://arxiv.org/abs/2203.05174_.
+
+    """
+    function _overlapped_subjects(cohorts, conn; patient_overlaps = :default)
+    
+        if patient_overlaps == :default
+            required_overlapping_phenotypes = ceil(length(cohorts) / 2)
+        end
 
         subjects = GetCohortSubjects(cohorts, conn)
         subjects.count = [count(==(subject), subjects.subject_id) for subject in subjects.subject_id]
@@ -47,6 +106,19 @@ module Fairness
 
     end
 
+    """
+    function demographic_parity(
+        cohorts, 
+        funcs, 
+        conn; 
+        labels = false, 
+        silver = false, 
+        reference_subjects = "", 
+        subset_length = 10000
+    )
+
+
+    """
     function demographic_parity(
         cohorts, 
         funcs, 
@@ -67,7 +139,11 @@ module Fairness
         end
     end
 
-    function _demographic_parity(cohorts::Vector{<:Any}, funcs, conn, reference_subjects, process_size, silver)
+    """
+    _demographic_parity(cohorts::Vector{<:Any}, funcs, conn, reference_subjects, subset_length, silver)
+
+    """
+    function _demographic_parity(cohorts::Vector{<:Any}, funcs, conn, reference_subjects, subset_length, silver)
 
         _funcs = [Fix2(fun, conn) for fun in funcs]
 
@@ -128,7 +204,11 @@ module Fairness
         return dps
     end
 
-    function _demographic_parity(cohorts::DataFrame, funcs, conn, reference_subjects, process_size, silver)
+    """
+    _demographic_parity(cohorts::DataFrame, funcs, conn, reference_subjects, subset_length, silver)
+
+    """
+    function _demographic_parity(cohorts::DataFrame, funcs, conn, reference_subjects, subset_length, silver)
 
         _funcs = [Fix2(fun, conn) for fun in funcs]
 
@@ -196,7 +276,11 @@ module Fairness
         return dps
     end
 
-    function _demographic_parity(cohorts::Vector{<:Any}, funcs, conn, reference_subjects, process_size)
+    """
+    _demographic_parity(cohorts::Vector{<:Any}, funcs, conn, reference_subjects, subset_length)
+
+    """
+    function _demographic_parity(cohorts::Vector{<:Any}, funcs, conn, reference_subjects, subset_length)
 
         _funcs = [Fix2(fun, conn) for fun in funcs]
 
@@ -235,7 +319,12 @@ module Fairness
         return dps
     end
 
-    function _demographic_parity(cohorts::DataFrame, funcs, conn, reference_subjects, process_size)
+    """
+    _demographic_parity(cohorts::DataFrame, funcs, conn, reference_subjects, subset_length)
+
+
+    """
+    function _demographic_parity(cohorts::DataFrame, funcs, conn, reference_subjects, subset_length)
 
         _funcs = [Fix2(fun, conn) for fun in funcs]
 
@@ -279,7 +368,11 @@ module Fairness
         return dps
     end
 
-    function equality_of_opportunity(cohorts, funcs, conn; reference_subjects = "", process_size = 10000)
+    """
+    equality_of_opportunity(cohorts, funcs, conn; reference_subjects = "", subset_length = 10000)
+
+    """
+    function equality_of_opportunity(cohorts, funcs, conn; reference_subjects = "", subset_length = 10000)
 
         _funcs = [Fix2(fun, conn) for fun in funcs]
 
@@ -324,7 +417,11 @@ module Fairness
         return eoo
     end
 
-    function predictive_rate_parity(cohorts, funcs, conn; reference_subjects = "", process_size = 10000)
+    """
+    predictive_rate_parity(cohorts, funcs, conn; reference_subjects = "", subset_length = 10000)
+
+    """
+    function predictive_rate_parity(cohorts, funcs, conn; reference_subjects = "", subset_length = 10000)
 
         _funcs = [Fix2(fun, conn) for fun in funcs]
 
